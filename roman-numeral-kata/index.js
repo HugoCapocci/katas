8322ac8486af0f2efcf721cb523264f9468ba145
Hugo Capocci <h.capocci@gmail.com>
2017-12-09 20:15:31 +0100

    First refacto working with big numbers

--- roman-numeral-kata/index.js
+++ roman-numeral-kata/index.js
@@ -1,14 +1,30 @@
 const refs = [
+  { num: 100, roman: 'C'},
+  { num: 90, roman: 'XC'},
+  { num: 50, roman: 'L'},
+  { num: 40, roman: 'XL'},
+  { num: 10, roman: 'X'},
+  { num: 9, roman: 'IX', exception: true},
   { num: 5, roman: 'V'},
-  { num: 4, roman: 'IV'},
+  { num: 4, roman: 'IV', exception: true},
   { num: 1, roman: 'I'},
 ]
 
 const toRomanNumber = (number) => {
-  for (let {num, roman} of refs) {
-    if (number === num) return roman;
-    if (number > num) return roman.repeat(number);
-  }
+  return compute(number, refs, 0);
 };
+
+const compute = (number, refs, index) => {
+  const { num, roman, exception } = refs[index];
+  if (number === num) return roman;
+  if (number > num && !exception) {
+    const occurences = parseInt(number / num);
+    const next = number - (num * occurences);
+    if (next > 0 && num > 1) {
+      return roman.repeat(occurences) + compute(next, refs, index + 1);
+    } else return roman.repeat(number);
+  }
+  return compute(number, refs, index + 1);
+}
 
 export default toRomanNumber;
--- roman-numeral-kata/test.js
+++ roman-numeral-kata/test.js
@@ -1,20 +1,31 @@
 import toRomanNumber from './index';
 
 describe('numberToRomanNumeral', () => {
-  it ('should convert 1 to I', () => {
+  it('should convert 1 to I', () => {
     expect(toRomanNumber(1)).toEqual('I');
   });
 
-  it ('should convert 2 to II', () => {
+  it('should convert 2 to II', () => {
     expect(toRomanNumber(2)).toEqual('II');
   });
 
-  it ('should convert 4 to IV', () => {
+  it('should convert 4 to IV', () => {
     expect(toRomanNumber(4)).toEqual('IV');
   });
 
-  it ('should convert 5 to V', () => {
+  it('should convert 5 to V', () => {
     expect(toRomanNumber(5)).toEqual('V');
   });
 
+  it('should convert 7 to VII', () => {
+    expect(toRomanNumber(7)).toEqual('VII');
+  });
+
+  it('should convert 43 to XLIII', () => {
+    expect(toRomanNumber(43)).toEqual('XLIII');
+  });
+
+  it('should convert 369 to CCCLXIX', () => {
+    expect(toRomanNumber(369)).toEqual('CCCLXIX');
+  });
 });
