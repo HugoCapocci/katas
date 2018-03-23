f612d94bee94b30693e00a5524bd253719a18239
Hugo CAPOCCI <hcapocci@degetel.com>
2018-03-23 14:32:19 +0100

    First IV rules implemented

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,3 +1,4 @@
+const players = ['X', 'O'];
 
 export const getGrid= () => {
     return [
@@ -7,4 +8,35 @@ export const getGrid= () => {
     ];
 }
 
+const isBoardFull = (grid) => {
+    return grid.every((row) => {
+        return row.every((field) => {
+            return field !== '';
+        });
+    })
+}
 
+const isRowTakenByAPlayer = (grid) => {
+    for(let i = 0; i < grid.length; i++){
+        let row = grid[i];
+        let isFirstPlayer = row.every((field) => {
+            return field === players[0]
+        });
+
+        let isSecondPlayer = row.every((field) => {
+            return field === players[1]
+        });
+
+        if (isFirstPlayer || isSecondPlayer)
+            return true;
+    }
+    return false;
+}
+
+export const isFinished= (grid) => {
+    return isBoardFull(grid) || isRowTakenByAPlayer(grid);
+}
+
+export const getPlayer= (playerOrder) => {
+    return players[playerOrder];
+}
