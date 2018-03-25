2bdac3b9b502d95e42392c1123fd911894ade98c
Hugo CAPOCCI <hcapocci@degetel.com>
2018-03-25 14:19:21 +0200

    Use class on top of the game

--- tic-tac-toe/test.js
+++ tic-tac-toe/test.js
@@ -1,11 +1,14 @@
-import { getGrid, isFinished, getPlayer } from './index';
+import TicTacToeGame from './index';
 
 describe.only('ticTacToc', () => {
+
+  const ticTacToeGame = new TicTacToeGame();
+
   it('Should has nine fields in a 3x3 grid', () => {
-    expect(getGrid().length).toEqual(3);
-    expect(getGrid()[0].length).toEqual(3);
-    expect(getGrid()[1].length).toEqual(3);
-    expect(getGrid()[2].length).toEqual(3);
+    expect(ticTacToeGame.getGrid().length).toEqual(3);
+    expect(ticTacToeGame.getGrid()[0].length).toEqual(3);
+    expect(ticTacToeGame.getGrid()[1].length).toEqual(3);
+    expect(ticTacToeGame.getGrid()[2].length).toEqual(3);
   });
 
   it('Should be over when all fields are taken', () => {
@@ -14,21 +17,16 @@ describe.only('ticTacToc', () => {
       ['b','b','b'],
       ['b','b','b'],
     ]
-    expect(isFinished(fullGrid)).toEqual(true);
+    expect(ticTacToeGame.isFinished(fullGrid)).toEqual(true);
   });
 
   it('Should not be over when all fields are not taken', () => {
-    const fullGrid = [
-      ['','b','b'],
-      ['b','b','b'],
-      ['b','b','b'],
-    ];
-    expect(isFinished(fullGrid)).toEqual(false);
+    expect(ticTacToeGame.isFinished()).toEqual(false);
   });
 
   it('Should have two players in the game (X and O)', () => {
-    expect(getPlayer(0)).toEqual('X');
-    expect(getPlayer(1)).toEqual('O');
+    expect(ticTacToeGame.getPlayer(0)).toEqual('X');
+    expect(ticTacToeGame.getPlayer(1)).toEqual('O');
   });
 
   it('Should be over when all fields in a row are taken by a player', () => {
@@ -37,7 +35,7 @@ describe.only('ticTacToc', () => {
       ['','',''],
       ['','',''],
     ];
-    expect(isFinished(fullGrid)).toEqual(true);
+    expect(ticTacToeGame.isFinished(fullGrid)).toEqual(true);
   });
 
   it('Should not be over when all fields in a row are not taken by a player', () => {
@@ -46,7 +44,7 @@ describe.only('ticTacToc', () => {
       ['','',''],
       ['','',''],
     ];
-    expect(isFinished(fullGrid)).toEqual(false);
+    expect(ticTacToeGame.isFinished(fullGrid)).toEqual(false);
   });
 
 });
