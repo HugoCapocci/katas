69498ceba7ec408947b84feaf779634e682ea295
Hugo CAPOCCI <hcapocci@degetel.com>
2018-03-25 13:49:15 +0200

    Fix indents

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,42 +1,42 @@
 const players = ['X', 'O'];
 
 export const getGrid= () => {
-    return [
-        ['','',''],
-        ['','',''],
-        ['','',''],
-    ];
+  return [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+  ];
 }
 
 const isBoardFull = (grid) => {
-    return grid.every((row) => {
-        return row.every((field) => {
-            return field !== '';
-        });
-    })
+  return grid.every((row) => {
+    return row.every((field) => {
+      return field !== '';
+    });
+  })
 }
 
 const isRowTakenByAPlayer = (grid) => {
-    for(let i = 0; i < grid.length; i++){
-        let row = grid[i];
-        let isFirstPlayer = row.every((field) => {
-            return field === players[0]
-        });
+  for(let i = 0; i < grid.length; i++){
+    let row = grid[i];
+    let isFirstPlayer = row.every((field) => {
+      return field === players[0]
+    });
 
-        let isSecondPlayer = row.every((field) => {
-            return field === players[1]
-        });
+    let isSecondPlayer = row.every((field) => {
+      return field === players[1]
+    });
 
-        if (isFirstPlayer || isSecondPlayer)
-            return true;
-    }
-    return false;
+    if (isFirstPlayer || isSecondPlayer)
+      return true;
+  }
+  return false;
 }
 
 export const isFinished= (grid) => {
-    return isBoardFull(grid) || isRowTakenByAPlayer(grid);
+  return isBoardFull(grid) || isRowTakenByAPlayer(grid);
 }
 
 export const getPlayer= (playerOrder) => {
-    return players[playerOrder];
+  return players[playerOrder];
 }
